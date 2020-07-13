245ae3cc45b54d6e405604f7adab9ecdad598a88
Douglas Mendes Assunção <capudougmor@gmail.com>
2020-07-13 22:52:50 +0000

    Etapa de destructuring em 21-exercicio em ConceitosBasicos

--- ProgramadorABordo/ConceitosBasicos/21-exercicio/src/components/UserList.js
+++ ProgramadorABordo/ConceitosBasicos/21-exercicio/src/components/UserList.js
@@ -1,8 +1,8 @@
 /*
   TODO:
-  1 - Passar para template strings na listagem de usuários
-  2 - Substituir "for" por "map"
-  3 - Utilizar destructuring na extração das propriedades de cada usuário com let
+  1 - Passar para template strings na listagem de usuários *
+  2 - Substituir "for" por "map" *
+  3 - Utilizar destructuring na extração das propriedades de cada usuário com let *
   4 - Utilizar valor de fallback caso não tenha valor de telefone no usuáriopara exibir o texto 'Não cadastrado'
   5 - Trocar if (status) por operador ternário
   6 - Utilizar função auxiliar para tratar o status
@@ -14,13 +14,8 @@
 */
 
 export default function renderUserList(userData) {
-  var usersHTML = '';
-
-  for (var i = 0; i < userData.length; i++) {
-    var name = userData[i].name;
-    var email = userData[i].email;
-    var telefone = userData[i].telefone;
-    var status = userData[i].status;
+  const usersHTML = userData.map(function(user, userIndex) {
+    let {name, email, telefone, status} = user;
 
     if (status) {
       status = 'Ativado';
@@ -28,35 +23,35 @@ export default function renderUserList(userData) {
       status = 'Desativado';
     }
 
-    usersHTML += (
-      '<tr>' +
-        '<th scope="row">' + (i + 1) + '</th>'+
-        '<td>' + name + '</td>' +
-        '<td>' + email + '</td>' +
-        '<td>' + telefone + '</td>' +
-        '<td>' + status + '</td>' +
-        '<td><button type="button" data-userId="'+ (i + 1) +'" class="btn btn-edit btn-link text-primary">Editar</button></td>' +
-        '<td><button type="button" class="btn btn-link text-danger">Excluir</button></td>' +
-      '</tr>'
-    );
-  }
+    return (
+      `<tr> 
+        <th scope="row">  ${(userIndex + 1)} </th>
+        <td> ${ name } </td>
+        <td> ${ email } </td>
+        <td> ${ telefone } </td>
+        <td> ${ status }</td>
+        <td><button type="button" data-userId="${(userIndex + 1)} " class="btn btn-edit btn-link text-primary">Editar</button></td>
+        <td><button type="button" class="btn btn-link text-danger">Excluir</button></td>
+      </tr>
+    `);
+  }).join('')
 
   return (
-    '<table class="table table-striped mt-4">' +
-      '<thead>' +
-        '<tr>' +
-          '<th scope="col">#</th>' +
-          '<th scope="col">Nome</th>' +
-          '<th scope="col">E-mail</th>' +
-          '<th scope="col">Telefone</th>' +
-          '<th scope="col">Status</th>' +
-          '<th scope="col">&nbsp;</th>' +
-          '<th scope="col">&nbsp;</th>' +
-        '</tr>' +
-      '</thead>' +
-      '<tbody>' +
-        usersHTML +
-      '</tbody>' +
-    '</table>'
-  )
+    `<table class="table table-striped mt-4">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Telefone</th>
+            <th scope="col">Status</th>
+            <th scope="col">&nbsp;</th>
+            <th scope="col">&nbsp;</th>
+        </tr>
+        </thead>
+        <tbody>
+        ${usersHTML}
+        </tbody>
+        </table>
+    `)
 };
